3b8c61e0d4f1df351fd897abad121cec715ffa72
Michael Dambacher <mdambacher@users.noreply.github.com>
2015-12-05 19:35:00 -0600

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit	: 10,
-  host  			: '52.88.112.1',  //change to local host
+  host  			: 'localhost',  //change to local host
   user  			: 'student',
   password			: 'default',
   database			: 'student'
