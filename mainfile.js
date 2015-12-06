960c15c83851a73a94fc2607f0c7b1b44d86ee03
Michael Dambacher <mdambacher@users.noreply.github.com>
2015-12-06 12:32:36 -0600

    Update mainfile.js

--- mainfile.js
+++ mainfile.js
@@ -86,7 +86,7 @@ app.get('/update', function(req,res,next){
 app.get('/updated',function(req,res,next){
   var context = {};
   mysql.pool.query('UPDATE workouts SET name=?, reps=?, weight=?, date=?, lbs=? WHERE id=?',
-    [req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.lbs, req.query.id],
+    [req.query.c, req.query.reps, req.query.weight, req.query.date, req.query.lbs, req.query.id],
     function(err, result){
     if(err){
       next(err);
